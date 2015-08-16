94ae72b138cb92821cd5e9fe70d1439b67fe899b
takashiki <857995137@qq.com>
2015-08-16 20:41:12 +0800

    add 115 play adjust

--- load.js
+++ load.js
@@ -1,3 +1,14 @@
+function getQueryString(name) {
+  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  var r = window.location.search.substr(1).match(reg);
+  var value = '';
+  if (r != null) {
+    value = decodeURI(r[2]);
+  }
+
+  return value;
+}
+
 $(function() {
   if ($('#bofqi embed').attr('quality') !== undefined) {
     $('#bofqi').css('height', 320);
@@ -5,5 +16,16 @@ $(function() {
     $('#bofqi embed').css('height', 320);
     $('#bofqi embed').css('width', 480);
     $('#bofqi').css('margin', '20px 0px');
+  }
+  
+  if(window.location.host == '115.com' && getQueryString('ct') == 'play') {
+    setInterval(function() {
+      $('#video').css('height', 324);
+      $('#video').css('width', 574);
+    }, 3000);
+    $('.wrap,.video-container,.video-player').css('height', 324);
+    $('.video-container,.video-player').css('width', 574);
+    $('.video-container').css('min-height', 324);
+    $('.wrap').css('width', 584);
   }
 });
